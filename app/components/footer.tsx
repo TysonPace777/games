66ab168603e43f98252ceb0fe4c0b487c069efc5
tysonpace777 <32.tpace@gmail.com>
2026-04-02 13:42:52 -0600

    Removed second s

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,7 @@ import styles from './components.module.css';
 export default function Footer() {
     return (
         <div className={styles.footer}>
-            <h1>T Gamess</h1>
+            <h1>T Games</h1>
             <p>Developed by: Tyson Pace</p>
             <p>(I do not own any of the images or content in this website)</p>
         </div>
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,7 +3,7 @@ import styles from './components.module.css';
 export default function Header() {
     return (
         <div className={styles.header}>
-            <h1>T Gamess</h1>
+            <h1>T Games</h1>
         </div>
     );
 }
